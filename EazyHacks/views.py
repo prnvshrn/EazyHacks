129409db20ef499177807c49897510fa90ce4661
prnvshrn <prnvshrn@gmail.com>
2017-12-29 17:59:39 +0530

    Initial setup

---
+++ EazyHacks/views.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+def openLogin(request):
+    template = loader.get_template('login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
---
+++ static/react_components.js
@@ -0,0 +1,1 @@
+// This contains all the react code utilized by the application
