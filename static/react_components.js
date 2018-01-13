9492cba99bfcfd4a67f9230ac96ec482f647e466
prnvshrn <prnvshrn@gmail.com>
2018-01-13 12:03:16 -0700

    Organized Homepage image into React components

--- static/react_components.js
+++ static/react_components.js
@@ -1,11 +1,12 @@
 // This contains all the react code utilized by the application
 
-
 // Styles for React components
 var Width100 = {width:'100%'};
 var Width50 = {width:'50%'};
 var UpperMargin10 = {marginTop:'5px',textAlign:'center'};
-var Test = "{% csrf_token %}";
+var TextCenter = "{% csrf_token %}";
+var Width100Height250 = {width:'100%',height:'250px'}
+
 
 // Definition for React components
 var Login = React.createClass({
@@ -19,21 +20,51 @@ var Login = React.createClass({
 
 ReactDOM.render(<Login />,document.getElementById("login_component"));
 
-var ImageBox = React.createClass({
-    render:function(){
+function ImageBox (props){
         return(
-            <div>
                 <div className="thumbnail">
                     <a href="www.google.com">
-                        <img src={soocer_image} style={Width100} />
-                        <div className="caption">
-                            <p>{soocer_caption}</p>
+                        <img src={props.img_src} style={Width100Height250} />
+                        <div className="caption text-center">
+                            <p><h3>{props.img_caption}</h3></p>
                         </div>
                     </a>
+                </div>
+        );
+}
+
+class ImageBoxLayout extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <ImageBox img_src={soocer_image} img_caption={soocer_caption}/>
+                    </div>
+                    <div className="col-md-4">
+                        <ImageBox img_src={kitchen_image} img_caption={kitchen_caption}/>
+                    </div>
+                    <div className="col-md-4">
+                        <ImageBox img_src={fitness_image} img_caption={fitness_caption}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <ImageBox img_src={gaming_image} img_caption={gaming_caption}/>
+                    </div>
+                    <div className="col-md-4">
+                        <ImageBox img_src={dailylife_image} img_caption={dailylife_caption}/>
+                    </div>
+                    <div className="col-md-4">
+                        <ImageBox img_src={miscellaneous_image} img_caption={miscellaneous_caption}/>
+                    </div>
                 </div>
             </div>
         );
     }
-});
+}
 
-ReactDOM.render(<ImageBox />,document.getElementById("image_box"));
+ReactDOM.render(<ImageBoxLayout />,document.getElementById("image_box_layout"));
+
